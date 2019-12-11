745bcfaae762d55c7bd38a5022151eb8e11fefb3
Dragos Neagu <hello@dragosneagu.com>
2019-12-11 23:47:13 +0000

    Updates to the Second Day of Advent of Code 2019.

--- 2/IntProgram.swift
+++ 2/IntProgram.swift
@@ -2,12 +2,36 @@
 
 import Foundation
 
+struct Operation {
+    var operation: Int
+    var noun: Int
+    var verb: Int
+    var destinationIndex: Int
+
+    init(_ opCodes: [Int], startingIndex: Int) {
+        self.operation = opCodes[startingIndex]
+        self.noun = opCodes[startingIndex + 1]
+        self.verb = opCodes[startingIndex + 2]
+        self.destinationIndex = opCodes[startingIndex + 3]
+    }
+
+    func perform(with opCodes: [Int]) -> Int {
+        var performOperation: (Int, Int) -> Int
+        switch operation {
+            case 1: performOperation = (+)
+            case 2: performOperation = (*)
+            default: return 0
+        }
+        return performOperation(opCodes[noun], opCodes[verb])
+    }
+}
+
 func readIntProgram(from filePath: String) -> [Int]? {
     do {
         return try String(contentsOfFile: filePath, encoding: .utf8)
                         .filter({!$0.isNewline && !$0.isWhitespace})
                         .components(separatedBy: ",")
-                        .filter({ $0 != "" }).compactMap({ Int($0) })
+                        .compactMap({ Int($0) })
     } catch {
         print("Couldn't read file at \(filePath)")
         print(error)
@@ -15,59 +39,39 @@ func readIntProgram(from filePath: String) -> [Int]? {
     return nil
 }
 
-func runOperation(for opCodes: [Int], at index: Int, _ function: (Int, Int) -> Int) -> [Int] {
+func substituteNounAndVerbIn1202Program(for opCodes: [Int], _ noun: Int, _ verb: Int) -> [Int] {
     var updatedOpCodes = opCodes
-    let indexValue1 = updatedOpCodes[index+1]
-    let indexValue2 = updatedOpCodes[index+2]
-    let indexValue3 = updatedOpCodes[index+3]
-    updatedOpCodes[indexValue3] = function(updatedOpCodes[indexValue1], updatedOpCodes[indexValue2])
-    return updatedOpCodes
-}
-
-func apply1202Program(for opCodes: [Int], _ value1: Int, _ value2: Int) -> [Int] {
-    var updatedOpCodes = opCodes
-    if updatedOpCodes.indices.contains(1) && opCodes.indices.contains(2) {
-        updatedOpCodes[1] = value1
-        updatedOpCodes[2] = value2
+    if opCodes.indices.contains(1) && opCodes.indices.contains(2) {
+        updatedOpCodes[1] = noun
+        updatedOpCodes[2] = verb
     }
     return updatedOpCodes
 }
 
-func run(_ opCodes: [Int], _ value1: Int, _ value2: Int) -> Int {
-    var opCodes1202 = apply1202Program(for: opCodes, value1, value2)
+func run(_ opCodes: [Int], _ noun: Int, _ verb: Int) -> Int {
+    var opCodes1202 = substituteNounAndVerbIn1202Program(for: opCodes, noun, verb)
 
-    opCodeLoop: for index in stride(from: 0, to: opCodes1202.count.advanced(by: -1), by: 4) {
-        let operation = opCodes1202[index]
-        
-        switch operation {
-            case 1: 
-                opCodes1202 = runOperation(for: opCodes1202, at: index, +)
-            case 2:
-                opCodes1202 = runOperation(for: opCodes1202, at: index, *)
-            case 99:
-                break opCodeLoop
-            default:
-                break
+    for index in stride(from: 0, to: opCodes1202.count.advanced(by: -1), by: 4) {
+        let operation = Operation(opCodes1202, startingIndex: index)
+        if [1,2].contains(operation.operation) {
+            opCodes1202[operation.destinationIndex] = operation.perform(with: opCodes1202)
         }
-
+        else if operation.operation == 99 {
+            break
+        }
     } 
     return opCodes1202[0]  
 }
 
 func findIndexes(for opCodes: [Int], matching value: Int) -> (noun: Int, verb: Int) {
-    var noun = 0
-    var verb = 0
-
-    bigLoop: for nounIndex in 0...99 {
-        for verbIndex in 00...99 {
+    for nounIndex in 0...99 {
+        for verbIndex in 0...99 {
             if run(opCodes, nounIndex, verbIndex) == value {
-                noun = nounIndex
-                verb = verbIndex
-                break bigLoop
+                return (nounIndex, verbIndex)
             }
         }
     }
-    return (noun, verb)
+    return (0, 0)
 }
 
 func runIntProgram() {
